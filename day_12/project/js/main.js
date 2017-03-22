eab1d68bf2d7317a6663bf3936cf9a057e0bf8f3
bhacker009 <sifathaque6@gmail.com>
2017-03-22 20:44:22 +0600

    added day 11 class

---
+++ day_12/project/js/main.js
@@ -0,0 +1,10 @@
+//
+//window.onload = function() {
+//	alert("hello");
+//}
+
+$("document").ready(function() {
+	$("p").css("border","5px solid green").css("color","#fff").css("background", "blue");
+	
+	
+});
